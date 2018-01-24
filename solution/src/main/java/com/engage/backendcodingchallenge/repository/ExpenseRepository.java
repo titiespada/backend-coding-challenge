9bce217b0e05dc3cea3ec70df7661590a03b1416
Patrícia Espada <pespada@opensoft.pt>
2018-01-24 16:08:13 +0000

    Mark repository with the required annotation.

--- solution/src/main/java/com/engage/backendcodingchallenge/repository/ExpenseRepository.java
+++ solution/src/main/java/com/engage/backendcodingchallenge/repository/ExpenseRepository.java
@@ -1,9 +1,11 @@
 package com.engage.backendcodingchallenge.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.engage.backendcodingchallenge.model.Expense;
 
+@Repository
 public interface ExpenseRepository extends CrudRepository<Expense, Integer> {
 
 }
