a8f93bf05849f77e0e3e35d5b4ab169df18b1f21
Patrícia Espada <pespada@opensoft.pt>
2018-01-26 16:25:09 +0000

    Fix duplicated string bug.

--- solution/src/main/java/com/engage/backendcodingchallenge/exception/RestResponseEntityExceptionHandler.java
+++ solution/src/main/java/com/engage/backendcodingchallenge/exception/RestResponseEntityExceptionHandler.java
@@ -21,31 +21,31 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     @ExceptionHandler(ExpenseIdNotFoundException.class)
     public ResponseEntity<ExceptionDto> expenseIdNotFound(ExpenseIdNotFoundException ex) {
         logger.error(ex.getMessage());
-        return new ResponseEntity<>(new ExceptionDto("Not Found", ex.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ExceptionDto(HttpStatus.NOT_FOUND.getReasonPhrase(), ex.getMessage()), HttpStatus.NOT_FOUND);
     }
     
     @ExceptionHandler(CurrencyCodeNotSupportedException.class)
     public ResponseEntity<ExceptionDto> currencyCodeNotSupported(CurrencyCodeNotSupportedException ex) {
         logger.error(ex.getMessage());
-        return new ResponseEntity<>(new ExceptionDto("Bad Request", ex.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ExceptionDto(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
     
     @ExceptionHandler(FormValidationException.class)
     public ResponseEntity<ExceptionDto> formInvalid(FormValidationException ex) {
         logger.error(ex.getMessage());
-        return new ResponseEntity<>(new ExceptionDto("Bad Request", ex.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ExceptionDto(HttpStatus.BAD_REQUEST.getReasonPhrase(), ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
     
     @ExceptionHandler({ NullPointerException.class, IllegalArgumentException.class, IllegalStateException.class })
     public ResponseEntity<ExceptionDto> handleInternal(final RuntimeException ex, final WebRequest request) {
         logger.error(ex.getMessage());
-        return new ResponseEntity<>(new ExceptionDto("Internal Server Error", ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(new ExceptionDto(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
         logger.error(ex.getMessage());
-        return handleExceptionInternal(ex, new ExceptionDto("Bad Request", "HTTP message not valid."), headers, HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(ex, new ExceptionDto(HttpStatus.BAD_REQUEST.getReasonPhrase(), "HTTP message not valid."), headers, HttpStatus.BAD_REQUEST, request);
     }
     
 }
